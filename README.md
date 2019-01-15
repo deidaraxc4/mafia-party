28b8ddd29b94a955ec6551a4981406301e289277
Nick Tang <nick.tang@onedatascan.com>
2019-01-15 15:46:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mafia Party
+A Spring Boot application on top of a Postgres database with Angular 6 serving the front end. Play the classic party game of Mafia with new roles.
---
+++ src/main/java/com/deidaraxc4/mafiaparty/MafiaPartyApplication.java
@@ -0,0 +1,14 @@
+package com.deidaraxc4.mafiaparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaPartyApplication.class, args);
+	}
+
+}
+
